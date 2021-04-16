7f84da273e3070e0086dfeff7bcd7b711fa79cee
ewamieszek <epaszkowska83@gmail.com>
2021-04-16 20:47:11 +0200

    move amount calculation to Movie class

--- src/main/java/Rental.java
+++ src/main/java/Rental.java
@@ -1,5 +1,8 @@
 public class Rental {
 
+    private Movie movie;
+    private int daysRented;
+
     public Rental(Movie movie, int daysRented) {
         this.movie = movie;
         this.daysRented = daysRented;
@@ -13,26 +16,8 @@ public class Rental {
         return movie;
     }
 
-    private Movie movie;
-    private int daysRented;
-
     public double amount() {
-        double thisAmount = 0;
-        switch (getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                thisAmount += 2;
-                if (getDaysRented() > 2)
-                    thisAmount += (getDaysRented() - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                thisAmount += getDaysRented() * 3;
-                break;
-            case Movie.CHILDRENS:
-                thisAmount += 1.5;
-                if (getDaysRented() > 3)
-                    thisAmount += (getDaysRented() - 3) * 1.5;
-                break;
-        }
-        return thisAmount;
+        return movie.amount(getDaysRented());
     }
+
 }
