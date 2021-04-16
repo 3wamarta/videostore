ffea89aa92c347821bb85ca0e9f6a2d0e3ca8992
ewamieszek <epaszkowska83@gmail.com>
2021-04-16 20:35:16 +0200

    move amount calculation to Rental class

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -23,26 +23,9 @@ public class Customer {
         String result = "Rental Record for " + getName() + "\n";
 
         for (Rental rental : rentals) {
-            double thisAmount = 0;
+			double thisAmount = rental.amount();
 
-            // determines the amount for each line
-            switch (rental.getMovie().getPriceCode()) {
-                case Movie.REGULAR:
-                    thisAmount += 2;
-                    if (rental.getDaysRented() > 2)
-                        thisAmount += (rental.getDaysRented() - 2) * 1.5;
-                    break;
-                case Movie.NEW_RELEASE:
-                    thisAmount += rental.getDaysRented() * 3;
-                    break;
-                case Movie.CHILDRENS:
-                    thisAmount += 1.5;
-                    if (rental.getDaysRented() > 3)
-                        thisAmount += (rental.getDaysRented() - 3) * 1.5;
-                    break;
-            }
-
-            frequentRenterPoints++;
+			frequentRenterPoints++;
 
             if (rental.getMovie().getPriceCode() == Movie.NEW_RELEASE
                     && rental.getDaysRented() > 1)
