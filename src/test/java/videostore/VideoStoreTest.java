5cf54c7dabda58f9e5f27acc18a37c53cdb6798d
ewamieszek <epaszkowska83@gmail.com>
2021-05-03 13:40:00 +0200

    correction in formatting

--- src/test/java/videostore/VideoStoreTest.java
+++ src/test/java/videostore/VideoStoreTest.java
@@ -22,7 +22,9 @@ public class VideoStoreTest {
 
     @Test
     public void testStatementWithoutRentals() {
-        assertEquals("Rental Record for Fred\n\nYou owed 0.0\nYou earned 0 frequent renter points\n", customer.statement());
+        assertEquals("Rental Record for Fred\n\n" +
+                        "You owed 0.0\nYou earned 0 frequent renter points\n",
+                customer.statement());
     }
 
     @ParameterizedTest
@@ -34,9 +36,11 @@ public class VideoStoreTest {
                 .ofMovie("The Cell")
                 .withPrice(NEW_RELEASE_PRICE)
                 .forDays(daysRented).build());
-        assertEquals("Rental Record for Fred\n\tThe Cell\t"+amount
-                +"\nYou owed "+amount+"\n" +
-                "You earned " + renterPoints+ " frequent renter points\n", customer.statement());
+        assertEquals("Rental Record for Fred\n" +
+                        "\tThe Cell\t" + amount +
+                        "\nYou owed " + amount + "\n" +
+                        "You earned " + renterPoints + " frequent renter points\n",
+                customer.statement());
     }
 
 
@@ -50,19 +54,26 @@ public class VideoStoreTest {
                 .ofMovie("The Tigger videostore.Movie")
                 .withPrice(NEW_RELEASE_PRICE)
                 .forDays(3).build());
-        assertEquals("Rental Record for Fred\n\tThe Cell\t9.0\n\tThe Tigger videostore.Movie\t9.0\nYou owed 18.0\nYou earned 4 frequent renter points\n", customer.statement());
+
+        assertEquals("Rental Record for Fred\n" +
+                        "\tThe Cell\t9.0\n\tThe Tigger videostore.Movie\t9.0\n" +
+                        "You owed 18.0\nYou earned 4 frequent renter points\n",
+                customer.statement());
     }
 
     @ParameterizedTest
     @CsvSource({"3, 1.5",
-                "4, 3.0",
-                "0, 1.5"})
+            "4, 3.0",
+            "0, 1.5"})
     public void testSingleChildrensStatement(int days, double amount) {
         customer.addRental(aRental()
                 .ofMovie("The Tigger videostore.Movie")
                 .withPrice(CHILDREN_PRICE)
                 .forDays(days).build());
-        assertEquals("Rental Record for Fred\n\tThe Tigger videostore.Movie\t"+ amount +"\nYou owed "+ amount +"\nYou earned 1 frequent renter points\n", customer.statement());
+        assertEquals("Rental Record for Fred\n" +
+                        "\tThe Tigger videostore.Movie\t" + amount + "\n" +
+                        "You owed " + amount + "\nYou earned 1 frequent renter points\n",
+                customer.statement());
     }
 
 
@@ -81,7 +92,10 @@ public class VideoStoreTest {
                 .withPrice(REGULAR_PRICE)
                 .forDays(3).build());
 
-        assertEquals("Rental Record for Fred\n\tPlan 9 from Outer Space\t2.0\n\t8 1/2\t2.0\n\tEraserhead\t3.5\nYou owed 7.5\nYou earned 3 frequent renter points\n", customer.statement());
+        assertEquals("Rental Record for Fred\n" +
+                        "\tPlan 9 from Outer Space\t2.0\n\t8 1/2\t2.0\n\tEraserhead\t3.5\n" +
+                        "You owed 7.5\nYou earned 3 frequent renter points\n",
+                customer.statement());
     }
 
 }
